a9233ded2c7b2188afb8bf3f2181c4b026dec4d2
roim <roim.rodrigo@gmail.com>
2021-05-18 16:36:01 -0700

    Clean up hello world example boilerplate

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,12 +1,8 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as vsc from 'vscode';
 
 suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
+  vsc.window.showInformationMessage('Start all tests.');
 
   test('Sample test', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
