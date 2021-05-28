a98181a4b8bfc80ccee1b6192d36b212f638ada8
roim <roim.rodrigo@gmail.com>
2021-05-27 20:13:24 -0700

    Handle openConfigInConsole edge cases

--- src/commands/openConfigInConsole.ts
+++ src/commands/openConfigInConsole.ts
@@ -1,7 +1,7 @@
 import * as vsc from 'vscode';
-import ProjectsState from '../state/ProjectsState';
+import ProjectsState, { StatsigConfig } from '../state/ProjectsState';
 
-export default function run(name?: string): void {
+export default async function run(name?: string): Promise<void> {
   if (!name) {
     return;
   }
@@ -12,14 +12,34 @@ export default function run(name?: string): void {
     return;
   }
 
-  if (configs.length > 1) {
-    void vsc.window.showErrorMessage(
-      `Multiple configs found for name ${name}.  Unfortunately, Rodrigo hasn't implemented config selection yet.`,
+  let config: StatsigConfig;
+  if (configs.length === 1) {
+    config = configs[0];
+  } else {
+    const projectName = await vsc.window.showQuickPick(
+      configs.map((p) => p.projectName),
+      {
+        title: 'Select a project',
+        canPickMany: false,
+      },
     );
-    return;
+
+    if (projectName === undefined) {
+      return;
+    }
+
+    const matchingConfig = configs.find((c) => c.projectName === projectName);
+    if (!matchingConfig) {
+      void vsc.window.showErrorMessage(
+        `No config with name ${name} in the selected Project. This is an extension bug, please report it.`,
+      );
+
+      return;
+    }
+
+    config = matchingConfig;
   }
 
-  const config = configs[0];
   const type = config.type === 'feature_gate' ? 'gates' : 'dynamic_configs';
   void vsc.env.openExternal(
     vsc.Uri.parse(
@@ -32,16 +52,44 @@ export function register(_context: vsc.ExtensionContext): vsc.Disposable {
   return vsc.commands.registerCommand(
     'statsig.openConfigInConsole',
     async (name?: string) => {
-      if (!name) {
-        name = await vsc.window.showInputBox({
+      if (name) {
+        await run(name);
+        return;
+      }
+
+      let prefill = undefined;
+      const selection = vsc.window.activeTextEditor?.selection;
+      if (selection) {
+        let range;
+        if (selection.end.isAfter(selection.start)) {
+          range = selection;
+        } else {
+          range = vsc.window.activeTextEditor?.document.getWordRangeAtPosition(
+            selection.end,
+          );
+        }
+
+        if (range && range.isSingleLine) {
+          prefill = vsc.window.activeTextEditor?.document.getText(range);
+        }
+      }
+
+      await run(
+        await vsc.window.showInputBox({
           title: 'Statsig: Open Config in Console',
           prompt:
             "Enter a config/gate name.  Hint: If your cursor is over a string, it'll be prefilled in.",
           placeHolder: 'enable_im_feeling_lucky',
-        });
-      }
+          value: prefill,
+          validateInput: (value: string) => {
+            if (ProjectsState.instance.findConfig(value)) {
+              return null;
+            }
 
-      run(name);
+            return 'No config found with this name.';
+          },
+        }),
+      );
     },
   );
 }
