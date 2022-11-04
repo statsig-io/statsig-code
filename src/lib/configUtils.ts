05a0add07fd8cb9487715c3c33512142e0ef50d1
Kenny Yi <kenny@statsig.com>
2022-11-04 14:53:46 -0700

    update contract

--- src/lib/configUtils.ts
+++ src/lib/configUtils.ts
@@ -1,6 +1,7 @@
 import * as vsc from 'vscode';
-import { APIConfigEntity, APIConfigRule } from '../contracts/projects';
+import { APIConfigRule } from '../contracts/projects';
 import { StatsigConfig } from '../state/ProjectsState';
+import ProjectsState from '../state/ProjectsState';
 import { getTierPrefix } from './webUtils';
 
 export function getConfigUrl(c: StatsigConfig): vsc.Uri {
@@ -27,8 +28,13 @@ function isPublic(rule: APIConfigRule): boolean {
   return rule.conditions[0].type === 'public';
 }
 
-function isStale(config: APIConfigEntity): boolean {
-  return config.extraData.checksInPast30Days === 0;
+export function getStaleConfig(configName: string): StatsigConfig[] {
+  const configs = getConfigsFromName(configName);
+  return configs.filter((config) => config.data.checksInPast30Days === 0);
+}
+
+export function getConfigsFromName(configName: string): StatsigConfig[] {
+  return ProjectsState.instance.findConfig(configName);
 }
 
 export type StaticConfigResult = 'pass' | 'fail' | 'mixed';
@@ -118,8 +124,8 @@ export function renderConfigInMarkdown(
     )}\n\`\`\``;
   }
 
-  if (c.data.extraData.checksInPast30Days !== undefined) {
-    body = `${body} \n\n Checks in past 30 days: ${c.data.extraData.checksInPast30Days}`;
+  if (c.data.checksInPast30Days !== undefined) {
+    body = `${body} \n\n Checks in past 30 days: ${c.data.checksInPast30Days}`;
   }
 
   return new vsc.MarkdownString(
