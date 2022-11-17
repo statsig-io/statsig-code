a9d956885785063e62868c4d29a3cba3839ec26c
Kenny Yi <kenny@statsig.com>
2022-11-16 16:31:51 -0800

    support diagnostics for more languages

--- src/lib/languageUtils.ts
+++ src/lib/languageUtils.ts
@@ -1,9 +1,26 @@
 export const CONFIG_NAME_REGEX = /[a-zA-Z0-9_ ]+/g;
 export const CONFIG_NAME_WITH_QUOTES_REGEX = /["'`][a-zA-Z0-9_ ]+["'`]/g;
 
-const SUPPORTED_LANGUAGES = ['javascript', 'typescript'] as const;
+// https://code.visualstudio.com/docs/languages/identifiers
+const SUPPORTED_LANGUAGES = [
+  'javascript',
+  'typescript',
+  'python',
+  'go',
+  'ruby',
+  'java',
+  'kotlin',
+] as const;
 export type SupportedLanguageType = typeof SUPPORTED_LANGUAGES[number];
-export const SUPPORTED_FILE_EXTENSIONS = ['js', 'ts'];
+export const SUPPORTED_FILE_EXTENSIONS = [
+  'js',
+  'ts',
+  'py',
+  'go',
+  'rb',
+  'java',
+  'kt',
+];
 
 export function isLanguageSupported(language: string): boolean {
   return (SUPPORTED_LANGUAGES as unknown as string[]).includes(language);
@@ -16,6 +33,31 @@ export function getStatsigAPICheckGateRegex(
   switch (language) {
     case 'javascript':
     case 'typescript':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]checkGate\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'python':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*check_gate\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'go':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*CheckGate\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'ruby':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*check_gate\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'java':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]checkGate(Async)?\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'kotlin':
       return new RegExp(
         `[a-zA-Z0-9_]+[.]checkGate\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
         'g',
@@ -34,6 +76,31 @@ export function getStatsigAPIGetConfigRegex(
         `[a-zA-Z0-9_]+[.]getConfig\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
         'g',
       );
+    case 'python':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]get_config\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'go':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*GetConfig\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'ruby':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*get_config\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'java':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]getConfig(Async)?\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'kotlin':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]getConfig\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
   }
 }
 
@@ -48,6 +115,31 @@ export function getStatsigAPIGetExperimentRegex(
         `[a-zA-Z0-9_]+[.]getExperiment\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
         'g',
       );
+    case 'python':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]get_experiment\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'go':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*GetExperiment\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'ruby':
+      return new RegExp(
+        `[a-zA-Z0-9_]*[.]*get_experiment\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'java':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]getExperiment(Async)?\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
+    case 'kotlin':
+      return new RegExp(
+        `[a-zA-Z0-9_]+[.]getExperiment\\([^)]*["'\`]${configName}["'\`][^)]*\\)`,
+        'g',
+      );
   }
 }
 
@@ -58,7 +150,13 @@ export function getVariableAssignmentRegex(
   switch (language) {
     case 'javascript':
     case 'typescript':
+    case 'python':
+    case 'ruby':
+    case 'java':
+    case 'kotlin':
       return new RegExp(`[=][\\s]?["'\`]${configName}["'\`]`, 'g');
+    case 'go':
+      return new RegExp(`(=|:=)[\\s]?["'\`]${configName}["'\`]`, 'g');
   }
 }
 
@@ -69,7 +167,13 @@ export function checkGateReplacement(
   switch (language) {
     case 'javascript':
     case 'typescript':
+    case 'go':
+    case 'ruby':
+    case 'java':
+    case 'kotlin':
       return String(value) ?? 'false';
+    case 'python':
+      return value === true ? 'True' : 'False';
   }
 }
 
@@ -80,7 +184,14 @@ export function getConfigReplacement(
   switch (language) {
     case 'javascript':
     case 'typescript':
+    case 'python':
+    case 'ruby':
       return String(value) ?? '{}';
+    case 'go':
+      return String(value) ?? 'nil';
+    case 'java':
+    case 'kotlin':
+      return String(value) ?? 'null';
   }
 }
 
@@ -91,7 +202,14 @@ export function getExperimentReplacement(
   switch (language) {
     case 'javascript':
     case 'typescript':
+    case 'python':
+    case 'ruby':
       return String(value) ?? '{}';
+    case 'go':
+      return String(value) ?? 'nil';
+    case 'java':
+    case 'kotlin':
+      return String(value) ?? 'null';
   }
 }
 
